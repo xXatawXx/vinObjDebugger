8001b50d0ecb25a0e9ef1b519ae483720c009aa9
Vincent <57841743+xXatawXx@users.noreply.github.com>
2023-06-03 16:51:12 -0500

    Update fxmanifest.lua
    add config crap

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,5 +4,5 @@ author 'Vinny'
 version '1.0'
 description 'Real time Rotation and Position XYZ editor for objects.'
 
-client_script 'core/client/*.js'
+client_scripts {'core/_config.js', 'core/client/*.js'}
 server_script 'core/server/*.lua'
