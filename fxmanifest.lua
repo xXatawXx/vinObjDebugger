224a85f4dd8ddeff148e7928410f0e2c10789dcf
Vincent <57841743+xXatawXx@users.noreply.github.com>
2023-06-05 15:01:55 -0500

    Update fxmanifest.lua
    add server manifest for js

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,4 +5,4 @@ version '1.0'
 description 'Real time Rotation and Position XYZ editor for objects.'
 
 client_scripts {'core/_config.js', 'core/client/*.js'}
-server_script 'core/server/*.lua'
+server_script 'core/server/sv_main.js'
